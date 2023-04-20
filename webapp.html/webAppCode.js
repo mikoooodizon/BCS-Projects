ef915f8996102ce7b7d7781cf1cf55cca721fe0f
mikoooodizon <mikooodizon@gmail.com>
2023-04-20 12:51:17 +0200

    teething

--- webapp.html/webAppCode.js
+++ webapp.html/webAppCode.js
@@ -1,18 +1,17 @@
 function calculate() {
-    const income = parseFloat(document.getElementById('incomeInput').value.replace(/,/g, ''));
-    const expenses = parseFloat(document.getElementById('expensesInput').value.replace(/,/g, ''));
-    const spend = parseFloat(document.getElementById('spendInput').value.replace(/,/g, ''));
-    
-    const projectedSpend = spend * Math.pow(1.06, 20);
-    const result = projectedSpend * 20 / (income - expenses);
-    
-    document.getElementById('result').textContent = result.toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-    
-    return result ;
-  }
+  const income = parseFloat(document.getElementById('incomeInput').value.replace(/,/g, ''));
+  const expenses = parseFloat(document.getElementById('expensesInput').value.replace(/,/g, ''));
+  const spend = parseFloat(document.getElementById('spendInput').value.replace(/,/g, ''));
   
+  const projectedSpend = spend * Math.pow(1.06, 30);
+  const result = projectedSpend * 30 / (income - expenses);
+  
+  document.getElementById('result').textContent = result.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  
+  return result;
+}

c8a2e56eca46950779c8e9541dea61e5c7cd1dd8
mikoooodizon <mikooodizon@gmail.com>
2023-04-20 12:53:52 +0200

    adjusted function and wording

--- webapp.html/webAppCode.js
+++ webapp.html/webAppCode.js
@@ -3,15 +3,30 @@ function calculate() {
   const expenses = parseFloat(document.getElementById('expensesInput').value.replace(/,/g, ''));
   const spend = parseFloat(document.getElementById('spendInput').value.replace(/,/g, ''));
   
-  const projectedSpend = spend * Math.pow(1.06, 30);
-  const result = projectedSpend * 30 / (income - expenses);
+  const growthRate = 1.06;
+  const projectionYears = 30;
+  let nestEgg = 0;
+  let remainingYears = 0;
   
-  document.getElementById('result').textContent = result.toLocaleString('en-US', {
+  for (let i = 1; i <= projectionYears; i++) {
+    nestEgg += spend / (Math.pow(growthRate, i));
+  }
+  
+  remainingYears = Math.ceil(Math.log(nestEgg / (income - expenses)) / Math.log(growthRate));
+  
+  const result = `With a 6% annual growth rate in your saving, you'll need to save up ${nestEgg.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })} to cover your projected annual spend of ${spend.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  });
+  })} for the next 30 years. You'll need to save for an additional ${remainingYears} years to reach this goal given you maintain annual income and expenses.`;
+  
+  document.getElementById('result').textContent = result;
   
   return result;
 }
